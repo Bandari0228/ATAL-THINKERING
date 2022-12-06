488655fa9fe7fd34280bdf909bd4c5ba32becdf3
Bandari0228 <88103590+Bandari0228@users.noreply.github.com>
2022-12-06 20:47:01 +0530

    Add files via upload

---
+++ EXPERIMENT-REPORT/index.js
@@ -0,0 +1,107 @@
+function fun2(){
+    
+    const btndiv=document.getElementById("btndiv2");
+    var division =document.createElement("div");
+    var input=document.createElement("input");
+    input.type="text";
+    division.appendChild(input);
+    btndiv2.appendChild(division);
+     
+}
+
+function dfun2(){
+    const btndiv = document.getElementById('btndiv2');
+    let last = btndiv.childNodes[btndiv.childNodes.length-1];
+    last.parentNode.removeChild(last);
+}
+
+function fun3(){
+    
+    const btndiv=document.getElementById("btndiv3");
+    var division =document.createElement("div");
+    var input=document.createElement("input");
+    input.type="text";
+    division.appendChild(input);
+    btndiv3.appendChild(division);
+}
+
+function dfun3(){
+    const btndiv = document.getElementById('btndiv3');
+    let last = btndiv.childNodes[btndiv.childNodes.length-1];
+    last.parentNode.removeChild(last);
+}
+
+function fun4(){
+    
+    const btndiv=document.getElementById("btndiv4");
+    var division =document.createElement("div");
+    var input=document.createElement("input");
+    input.type="text";
+    division.appendChild(input);
+    btndiv4.appendChild(division);
+}
+
+function dfun4(){
+    const btndiv = document.getElementById('btndiv4');
+    let last = btndiv.childNodes[btndiv.childNodes.length-1];
+    last.parentNode.removeChild(last);
+}
+
+function fun5(){
+    
+    const btndiv=document.getElementById("btndiv5");
+    var division =document.createElement("div");
+    var input=document.createElement("input");
+    input.type="text";
+    division.appendChild(input);
+    btndiv5.appendChild(division);
+}
+
+function dfun5(){
+    const btndiv = document.getElementById('btndiv5');
+    let last = btndiv.childNodes[btndiv.childNodes.length-1];
+    last.parentNode.removeChild(last);
+}
+
+function fun6(){
+    
+    const btndiv=document.getElementById("btndiv6");
+    var division =document.createElement("div");
+    var input=document.createElement("input");
+    input.type="text";
+    division.appendChild(input);
+    btndiv6.appendChild(division);
+}
+function dfun6(){
+    const btndiv = document.getElementById('btndiv6');
+    let last = btndiv.childNodes[btndiv.childNodes.length-1];
+    last.parentNode.removeChild(last);
+}
+function fun7(){
+    
+    const btndiv=document.getElementById("btndiv7");
+    var division =document.createElement("div");
+    var input=document.createElement("input");
+    input.type="text";
+    division.appendChild(input);
+    btndiv7.appendChild(division);
+}
+function dfun7(){
+    const btndiv = document.getElementById('btndiv7');
+    let last = btndiv.childNodes[btndiv.childNodes.length-1];
+    last.parentNode.removeChild(last);
+}
+function fun8(){
+    
+    const btndiv=document.getElementById("btndiv8");
+    var division =document.createElement("div");
+    var input=document.createElement("input");
+    input.type="text";
+    division.appendChild(input);
+    btndiv8.appendChild(division);
+}
+function dfun8(){
+    const btndiv = document.getElementById('btndiv8');
+    let last = btndiv.childNodes[btndiv.childNodes.length-1];
+    last.parentNode.removeChild(last);
+}
